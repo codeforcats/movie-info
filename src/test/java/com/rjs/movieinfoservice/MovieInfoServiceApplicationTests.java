2c2ee2a7037b34a2e542ba508abf7e19f0786fc1
codeforcats <richard.spurr@gmail.com>
2022-05-25 16:55:29 +0100

    tinkered with test

--- src/test/java/com/rjs/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ src/test/java/com/rjs/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.rjs.movieinfoservice;
 
+import com.rjs.movieinfoservice.model.MovieInfo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MovieInfoServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldReturnHardCodedMovieInfo(){
+		MovieInfo movieInfo = restTemplate.getForObject("http://localhost:" + port + "/movies/1",
+				MovieInfo.class);
+
+		assertThat(movieInfo).isEqualTo(new MovieInfo(1, "Jaws"));
+	}
 }
