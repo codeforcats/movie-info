4be926c54209a9c1003fcaebb69555f4a9b6c8a3
codeforcats <richard.spurr@gmail.com>
2022-05-30 11:58:20 +0100

    tidied up unit test

--- src/test/java/com/rjs/movieinfoservice/MovieControllerTest.java
+++ src/test/java/com/rjs/movieinfoservice/MovieControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,17 +21,19 @@ class MovieControllerTest {
     MovieInfoRepo mockMovieInfoRepo;
 
     @Test
-    void MovieInfoFound() throws Exception {
+    void movieFound() throws Exception {
 
         when(mockMovieInfoRepo.getMovieInfo("Good News"))
                 .thenReturn(new MovieInfo("Good News", "A good news story."));
 
         mockMvc.perform(get("/movies/Good News"))
-               .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"movieId\":\"Good News\",\"description\":\"A good news story.\"}"));;
     }
 
     @Test
-    void StatusNotFound() throws Exception {
+    void movieNotFound() throws Exception {
 
         when(mockMovieInfoRepo.getMovieInfo("Bad News"))
                 .thenThrow(new ReourceNotFoundException());
@@ -38,4 +41,5 @@ class MovieControllerTest {
         mockMvc.perform(get("/movies/Bad News"))
                 .andExpect(status().isNotFound());
     }
+
 }
